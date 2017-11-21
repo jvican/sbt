08e24c0962ecb63843cd5898b7c05d92ecf26c11
Martin Duhem <martin.duhem@gmail.com>
2017-11-21 10:59:45 +0100

    Hardcoded stuff

---
+++ internal/util-collection/target/scala-2.12/src_managed/main/sbt/internal/util/ScalaKeywords.scala
@@ -0,0 +1,4 @@
+package sbt.internal.util
+object ScalaKeywords {
+  val values = Set("yield", "private", "null", "while", "<:", "object", "abstract", "<%", "catch", "#", ":", "type", "final", "then", "=>", "override", "_", "import", "macro", "var", "protected", "sealed", "def", "val", "super", "new", "trait", "package", "lazy", "if", ".", "forSome", ">:", "false", "=", "this", "@", "implicit", "throw", "true", "match", "class", "return", "<-", "try", "finally", "case", "with", "extends", "for", "do", "else")
+}
